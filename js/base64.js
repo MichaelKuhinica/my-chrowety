ed651d9c665af96a585723b2f911fb4c4009453c
leodutra.br <leodutra.br@f1cf3d9a-e38e-11de-a6a9-d5aeed48c2a6>
2009-12-29 06:41:52 +0000

    [Branch leodutra] Optimized base64.js
    git-svn-id: http://chrowety.googlecode.com/svn/trunk@45 f1cf3d9a-e38e-11de-a6a9-d5aeed48c2a6

--- js/base64.js
+++ js/base64.js
@@ -1,46 +1,54 @@
-function btoa(str) {
-        var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-        var encoded = [];
-        var c = 0;
-        while (c < str.length) {
-            var b0 = str.charCodeAt(c++);
-            var b1 = str.charCodeAt(c++);
-            var b2 = str.charCodeAt(c++);
-            var buf = (b0 << 16) + ((b1 || 0) << 8) + (b2 || 0);
-            var i0 = (buf & (63 << 18)) >> 18;
-            var i1 = (buf & (63 << 12)) >> 12;
-            var i2 = isNaN(b1) ? 64 : (buf & (63 << 6)) >> 6;
-            var i3 = isNaN(b2) ? 64 : (buf & 63);
-            encoded[encoded.length] = chars.charAt(i0);
-            encoded[encoded.length] = chars.charAt(i1);
-            encoded[encoded.length] = chars.charAt(i2);
-            encoded[encoded.length] = chars.charAt(i3);
-        }
-        return encoded.join('');
+// optimized by leodutra.br (2009y.12m.29d)
+
+function btoa(str){
+    var CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+    var L = str.length;
+    var ENCODED = [];
+    var n = 0; // encoded index, optimization
+    var c = 0;
+    var b1;
+    var b2;
+    var buf;
+    while (c < L) {
+        buf = ((str.charCodeAt(c)) << 16) + ((b1 = str.charCodeAt(c + 1)) << 8 || 0) + ((b2 = str.charCodeAt(c + 2)) || 0);
+        c += 3; // optimization
+        ENCODED[n] = CHARS.charAt((buf & (63 << 18)) >> 18);
+        ENCODED[n + 1] = CHARS.charAt((buf & (63 << 12)) >> 12);
+        ENCODED[n + 2] = CHARS.charAt(isNaN(b1) ? 64 : (buf & (63 << 6)) >> 6);
+        ENCODED[n + 3] = CHARS.charAt(isNaN(b2) ? 64 : (buf & 63));
+        n += 4; //opt
     }
-    function atob(str) {
-        var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-        var invalid = {
-            strlen: (str.length % 4 != 0),
-            chars:  new RegExp('[^' + chars + ']').test(str),
-            equals: (/=/.test(str) && (/=[^=]/.test(str) || /={3}/.test(str)))
-        };
-        if (invalid.strlen || invalid.chars || invalid.equals)
-            throw new Error('Invalid base64 data');
-        var decoded = [];
-        var c = 0;
-        while (c < str.length) {
-            var i0 = chars.indexOf(str.charAt(c++));
-            var i1 = chars.indexOf(str.charAt(c++));
-            var i2 = chars.indexOf(str.charAt(c++));
-            var i3 = chars.indexOf(str.charAt(c++));
-            var buf = (i0 << 18) + (i1 << 12) + ((i2 & 63) << 6) + (i3 & 63);
-            var b0 = (buf & (255 << 16)) >> 16;
-            var b1 = (i2 == 64) ? -1 : (buf & (255 << 8)) >> 8;
-            var b2 = (i3 == 64) ? -1 : (buf & 255);
-            decoded[decoded.length] = String.fromCharCode(b0);
-            if (b1 >= 0) decoded[decoded.length] = String.fromCharCode(b1);
-            if (b2 >= 0) decoded[decoded.length] = String.fromCharCode(b2);
-        }
-        return decoded.join('');
+    return ENCODED.join('');
+}
+
+function atob(str){
+    var CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+    if (str.length % 4 !== 0 || (new RegExp('[^' + CHARS + ']')).test(str) || (/\=/.test(str) && ((/\=[^\=]/).test(str) || (/\={3}/).test(str)))) {
+        throw new Error('Invalid base64 data');
     }
+    var L = str.length;
+    var DECODED = [];
+    var n = 0; // DECODED index, opt
+    var c = 0;
+    var i1;
+    var i2;
+    var i3;
+    var buf;
+    var b1;
+    var b2;
+    while (c < L) {
+        i1 = CHARS.indexOf(str.charAt(c + 1));
+        i2 = CHARS.indexOf(str.charAt(c + 2));
+        i3 = CHARS.indexOf(str.charAt(c + 3));
+        buf = (CHARS.indexOf(str.charAt(c)) << 18) + (i1 << 12) + ((i2 & 63) << 6) + (i3 & 63);
+        c += 4; // opt
+        DECODED[n++] = String.fromCharCode((buf & (255 << 16)) >> 16);
+        if ((b1 = (i2 == 64) ? false : -1 < (buf & (255 << 8)) >> 8)) { //  -1 < is faster than >= 0
+            DECODED[n++] = String.fromCharCode(b1);
+        }
+        if ((b2 = (i3 == 64) ? false : -1 < (buf & 255))) { // -1 < is faster than >= 0
+            DECODED[n++] = String.fromCharCode(b2);
+        }
+    }
+    return DECODED.join('');
+}
